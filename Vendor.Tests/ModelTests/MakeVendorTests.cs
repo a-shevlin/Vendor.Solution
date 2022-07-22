73efb2f5e992250321e4bdf1bcf06826fb902334
Alex Shevlin <alexshevlin1@gmail.com>
2022-07-22 11:11:27 -0700

    add AddOrder test

--- Vendor.Tests/ModelTests/MakeVendorTests.cs
+++ Vendor.Tests/ModelTests/MakeVendorTests.cs
@@ -1,18 +1,34 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
 using Vendor.Models;
 using Vendor;
 
 namespace Vendor.Tests
 {
   [TestClass]
-  public class MakeVendorTests
+  public class MakeVendorTests : IDisposable
   {
+    public void Dispose()
+    {
+      MakeVendor.ClearAll();
+    }
+
     // Test methods go here
     [TestMethod]
-    public void MakeVendor_CreateInstanceOfVendor_ReturnValue()
+    public void MakeVendor_CreateInstanceOfVendor_MakeVendor()
     {
       MakeVendor newVendor = new MakeVendor("name", "description");
       Assert.AreEqual(typeof(MakeVendor), newVendor.GetType());
+    }
+
+    [TestMethod]
+    public void AddOrder_AddOrderToOrderList_List()
+    {
+      MakeVendor newVendor = new MakeVendor("name", "description");
+      Order newOrder = new Order("test", "test", 0, "date");
+      newVendor.AddOrder(newOrder);
+      Assert.AreEqual(1, newVendor.Orders.Count);
     }
   }
 }
