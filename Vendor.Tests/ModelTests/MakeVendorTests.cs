8b5281db550711bfc466fe75553ade0718e954c7
Alex Shevlin <alexshevlin1@gmail.com>
2022-07-22 11:21:11 -0700

    add GetList, FindVendor, and RemoveVendor tests.

--- Vendor.Tests/ModelTests/MakeVendorTests.cs
+++ Vendor.Tests/ModelTests/MakeVendorTests.cs
@@ -23,12 +23,39 @@ namespace Vendor.Tests
     }
 
     [TestMethod]
+    public void GetList_ReadList_List()
+    {
+      List<MakeVendor> empty = new List<MakeVendor> {};
+      List<MakeVendor> test = MakeVendor.GetList();
+      CollectionAssert.AreEqual(test, empty);
+    }
+
+    [TestMethod]
     public void AddOrder_AddOrderToOrderList_List()
     {
       MakeVendor newVendor = new MakeVendor("name", "description");
       Order newOrder = new Order("test", "test", 0, "date");
       newVendor.AddOrder(newOrder);
       Assert.AreEqual(1, newVendor.Orders.Count);
+    }
+
+    [TestMethod]
+    public void FindVendor_FindVendorById_Vendor()
+    {
+      MakeVendor newVendor = new MakeVendor("name", "description");
+      List<MakeVendor> test = MakeVendor.GetList();
+      Assert.AreEqual(MakeVendor.FindVendor(1), test[0]);
+    }
+
+    [TestMethod]
+    public void RemoveVendor_DeleteVendorFromList_List()
+    {
+      MakeVendor newVendor = new MakeVendor("name", "description");
+      MakeVendor newTwo = new MakeVendor("name", "description");
+      List<MakeVendor> test = MakeVendor.GetList();
+      List<MakeVendor> newList = new List<MakeVendor> {};
+      MakeVendor.RemoveVendor(1);
+      Assert.AreEqual(1, test.Count);
     }
   }
 }
